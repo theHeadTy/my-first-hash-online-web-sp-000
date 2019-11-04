386913fe3c32c59b83c57e106af26f6ecbf478cb
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 19:11:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,7 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   
-  shipping_manifest.values_at('oil paintings')
+  shipping_manifest['oil paintings']
 
 end
 
