18dee106305f965844721edd68cc0403e84be325
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 19:05:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  {}
+  {
+    h1: 'hello',
+    h2: 'hello2'
+  }
   
 end
 
